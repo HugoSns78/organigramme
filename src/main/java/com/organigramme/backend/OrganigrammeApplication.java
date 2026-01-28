38ea68ab8f4b9d5478ebf4af528378f09ad11558
Hugo  Sanson <hugo.sanson@student.junia.com>
2026-01-28 15:25:08 +0100

    Init Spring Boot project

---
+++ src/main/java/com/organigramme/backend/OrganigrammeApplication.java
@@ -0,0 +1,13 @@
+package com.organigramme.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganigrammeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganigrammeApplication.class, args);
+	}
+
+}
