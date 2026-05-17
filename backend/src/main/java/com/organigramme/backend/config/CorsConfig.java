1a537dce057e63cda66801825fb7108799c89be1
Hugo  Sanson <hugo.sanson@student.junia.com>
2026-05-17 19:08:08 +0200

    recreate angular frontend clean

--- backend/src/main/java/com/organigramme/backend/config/CorsConfig.java
+++ backend/src/main/java/com/organigramme/backend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.organigramme.backend.CORS;
+package com.organigramme.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
